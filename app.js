9f2bcb2f75dadd579d50900182324e621d2b5e54
Nabullah Ansari <nabullarock@gmail.com>
2023-06-03 18:08:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+const bodyParser = require("body-parser");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("*", cors());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Server Listen */
+app.listen(PORT, () => {
+  console.log("Server is running on Port ", PORT);
+});
+
+/* API Routers */
+// const userRouter = require("./router/core/userRouter");
+// app.use("/api/user", userRouter);
+module.exports = app;
