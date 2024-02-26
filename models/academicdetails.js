da649ba8dcfe3315fa647e6d51f92f517563e124
Nabullah Ansari <nansari@anviam.com>
2024-02-26 18:48:26 +0530

    fix database

--- models/academicdetails.js
+++ models/academicdetails.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
 		},
 		{
 			sequelize,
-			modelName: "academicDetails",
+			modelName: "academicdetails",
 		}
 	);
 	return academicDetails;
--- models/otp.js
+++ models/otp.js
@@ -27,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
 		},
 		{
 			sequelize,
-			modelName: "OTP",
+			modelName: "otps",
 		}
 	);
 	return OTP;
