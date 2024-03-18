f97ad9c378e22a5ce858d73ef81752ac62b733ab
Nabullah Ansari <nansari@anviam.com>
2024-03-18 13:04:55 +0530

    changes

--- controller/core/upload_files.js
+++ controller/core/upload_files.js
@@ -16,24 +16,39 @@ const upload_files = {
 				if (req.file) {
 					if (req.body.topic && req.body.subTopic && req.body.description) {
 						if (req.body.audienceRolesIds) {
-							const file = req.file;
-							// console.log(">>>>>file>>>>>", file);
-							// uploadToServer.uploadFile(file, function (data) {
-							// 	console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", data);
-							// });
-							const uploadFiles = await UploadFiles.create({
-								filePath: file.path.split("\\").slice(1).join("/"),
-								fileName: file.originalname,
-								fileType: file.originalname.substring(file.originalname.lastIndexOf("."), file.originalname.length),
-								audienceRolesIds: req.body.audienceRolesIds,
-								isPrivate: false,
-								topic: req.body.topic,
-								subTopic: req.body.subTopic,
-								description: req.body.description,
-								userId: req.userId,
-								tags: req.body.tags,
-								fileSize: file.size,
+							const uploadFiles = uploadToServer.uploadFile(req.file, async (data) => {
+								console.log(">>>>>file>>>>>", data);
+								if (data) {
+									const filesUploaded = await UploadFiles.create({
+										filePath: data,
+										fileName: req.file.originalname,
+										fileType: req.file.originalname.substring(req.file.originalname.lastIndexOf("."), req.file.originalname.length),
+										audienceRolesIds: req.body.audienceRolesIds,
+										isPrivate: false,
+										topic: req.body.topic,
+										subTopic: req.body.subTopic,
+										description: req.body.description,
+										userId: req.userId,
+										tags: req.body.tags,
+										fileSize: req.file.size,
+									});
+									console.log(">>>>>filesUploaded>>>>>", filesUploaded);
+									return filesUploaded;
+								}
 							});
+							// const uploadFiles = await UploadFiles.create({
+							// 	filePath: file.path.split("\\").slice(1).join("/"),
+							// 	fileName: file.originalname,
+							// 	fileType: file.originalname.substring(file.originalname.lastIndexOf("."), file.originalname.length),
+							// 	audienceRolesIds: req.body.audienceRolesIds,
+							// 	isPrivate: false,
+							// 	topic: req.body.topic,
+							// 	subTopic: req.body.subTopic,
+							// 	description: req.body.description,
+							// 	userId: req.userId,
+							// 	tags: req.body.tags,
+							// 	fileSize: file.size,
+							// });
 							if (!uploadFiles) {
 								return res.status(500).json({ status: false, message: "Your file could not be uploaded. Please try again." });
 							} else {
@@ -49,7 +64,7 @@ const upload_files = {
 								const notificationsDetails = { fileName: uploadFiles.fileName };
 								SEND_NOTIFICATION.sendNotification(req.userId, notificationsDetails, NOTIFICATION_TYPE.FILE_UPLOAD);
 								const encryptedFiles = CryptoJS.AES.encrypt(JSON.stringify(uploadFiles), process.env.SECRET_KEY).toString();
-								return res.status(200).json({ status: true, message: "File uploaded successfully.", data: encryptedFiles });
+								return res.status(200).json({ status: true, message: "File uploaded successfully.", data: uploadFiles });
 							}
 						} else {
 							return res.status(401).json({ status: false, message: "User's Role type is required.", data: [] });
