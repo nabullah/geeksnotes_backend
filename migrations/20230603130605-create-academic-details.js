0f3e1d8cf957c4af59441c7ea5c2f53b317cd745
Nabullah Ansari <nabullarock@gmail.com>
2024-04-16 20:43:22 +0530

    updated migrations

--- migrations/20230603130605-create-academic-details.js
+++ migrations/20230603130605-create-academic-details.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('academicDetails', {
+    await queryInterface.createTable('academicdetails', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -38,6 +38,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('academicDetails');
+    await queryInterface.dropTable('academicdetails');
   }
 };
--- migrations/20231220071922-create-otp.js
+++ migrations/20231220071922-create-otp.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		await queryInterface.createTable("OTPs", {
+		await queryInterface.createTable("otps", {
 			id: {
 				allowNull: false,
 				autoIncrement: true,
@@ -35,6 +35,6 @@ module.exports = {
 		});
 	},
 	async down(queryInterface, Sequelize) {
-		await queryInterface.dropTable("OTPs");
+		await queryInterface.dropTable("otps");
 	},
 };
